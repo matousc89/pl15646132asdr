4ac71c4730fa02837cefb7dbe1fa54ca83605873
Your Name <you@example.com>
2019-04-18 16:19:04 +0200

    some improvements

--- main.py
+++ main.py
@@ -19,10 +19,9 @@ def make_bold(a):
 CUT = "25"
 PATH = "data/data{}csv.csv".format(CUT)
 
+
 data = pd.read_csv(PATH)
-
 channels = [_ for _ in data.keys()if "TM" in _]
-
 
 # normalization
 z_score_params = {}
@@ -48,9 +47,9 @@ data = data.loc[max(windows):len(data)-max(windows)]
 results = []
 
 learning_curve = {}
+weights_stats = []
 
-for chidx, ch in enumerate(channels):
-    # print(ch)
+for chidx, ch in enumerate(channels[:]):
 
     # make the shifted channels
     target_keys = []
@@ -72,7 +71,7 @@ for chidx, ch in enumerate(channels):
     # pretrain
     x = sample[target_keys].values
     d = sample[ch].values
-    ntrain = 2000
+    ntrain = 20000
     learning_curve[ch] = []
     for _ in range(5):
         y, e, w = filt.run(d[:ntrain], x[:ntrain])
@@ -100,10 +99,7 @@ for chidx, ch in enumerate(channels):
     sample_rm = sample["{}_mean".format(name)] * std + mean
     e = (y[:] * std + mean) - (d[:] * std + mean)
 
-    # plt.plot(y[:] * std + mean)
-    # plt.show()
-
-
+    # calculate erros (MAE, MSE)
     MEASUREMENT_SAMPLE = 20000
     MSE = np.dot(e[-MEASUREMENT_SAMPLE:],e[-MEASUREMENT_SAMPLE:]) / MEASUREMENT_SAMPLE
     MAE = abs(e[-MEASUREMENT_SAMPLE:]).mean()
@@ -117,7 +113,9 @@ for chidx, ch in enumerate(channels):
     MAE_RM = rolling_mean.abs().mean()
 
 
-
+    """
+    Comparison table
+    """
     out_mae = make_bold(MAE) if MAE < MAE_F and MAE < MAE_RM else make_normal(MAE)
     out_maef = make_bold(MAE_F) if MAE_F < MAE and MAE_F < MAE_RM else make_normal(MAE_F)
     out_maerm = make_bold(MAE_RM) if MAE_RM < MAE and MAE_RM < MAE_F else make_normal(MAE_RM)
@@ -126,38 +124,51 @@ for chidx, ch in enumerate(channels):
     out_msef = make_bold(MSE_F) if MSE_F < MSE and MSE_F < MSE_RM else make_normal(MSE_F)
     out_mserm = make_bold(MSE_RM) if MSE_RM < MSE and MSE_RM < MSE_F else make_normal(MSE_RM)
 
-
     imp_mae = make_normal((1 - (MAE / min(MAE_RM, MAE_F))) * 100, r=2, fill=2)
-
-    # line = " & ".join([ch, out_mae, out_maef, out_maerm]) + r"\\"
-    # line = " & ".join([ch, out_mse, out_msef, out_mserm]) + r"\\"
 
     line = " & ".join([ch, out_mae, out_mse, out_maef, out_msef, out_maerm, out_mserm, imp_mae]) + r"\\"
 
     print(line)
 
 
-
-
-
-    # line = "{}\t{}\t{}\t{}".format(ch, MAE, MAE_F, MAE_RM)
-    # print(line)
-
-
-
-
-
-    # plt.plot(data[ch][e > 0.2].values, data[ref][e > 0.2].values, "r.")
-    # plt.plot(data[ch][e < -0.2].values, data[ref][e < -0.2].values, "b.")
-    # plt.show()
-
-    # plt.plot(data[channels[ref]], data[ch], ".")
-    # plt.show()
-
-
     error = abs(e)
 
     skip = 0
+
+    """
+    DETAIL look at the output
+    """
+    # LW = 1
+    # plt.plot(sample[ch].values[skip:], "k-", linewidth=LW*2, label="original target")
+    # plt.plot(y[skip:], "k-", linewidth=LW, label="Proposed- NLMS")
+    # plt.plot(sample["{}_mean".format(name)].values[skip:], "k--", linewidth=LW, label="Ref2 - rolling mean")
+    # plt.plot(sample[shift_name].values[skip:], "k:", linewidth=LW, label="Ref1 - follower")
+    # plt.ylabel("Normalized output [-]")
+    # plt.xlabel("k [-]")
+    # plt.xlim(29500, 30100)
+    # plt.tight_layout()
+    # plt.legend()
+    # plt.show()
+
+    
+    """
+    WEIGHTS adaptation
+    """
+    # plt.plot(w, "k")
+    # plt.ylabel("Weights [-]")
+    # plt.xlabel("k [-]")
+    # plt.tight_layout()
+    # plt.show()
+
+
+    """
+    WEIGHTS stats (mean, std)
+    """
+    weights_stats.append({
+        "name": ch,
+        "mean": w.mean(axis=0),
+        "std": w.std(axis=0),
+    })
 
 
     # plt.subplot(311)
@@ -172,7 +183,6 @@ for chidx, ch in enumerate(channels):
     # plt.plot(w[skip:])
     # plt.tight_layout()
     # plt.show()
-    #
 
 
 
@@ -206,6 +216,10 @@ for chidx, ch in enumerate(channels):
 
 # plt.show()
 
+
+"""
+Plot of learnings
+"""
 filename = "figs/learning{}.png".format(CUT)
 plt.figure(figsize=(6,3))
 for name, vals in learning_curve.items():
@@ -217,3 +231,23 @@ plt.tight_layout()
 plt.grid()
 plt.savefig(filename, dpi=300, bbox_inches=None)
 plt.clf()
+plt.close()
+
+
+"""
+Plot of weights
+"""
+filename = "figs/weights_distribution{}.png".format(CUT)
+plt.figure(figsize=(6,4))
+for idx, item in enumerate(weights_stats):
+    for n in range(len(item["mean"])):
+        hp = item["mean"][n] + item["std"][n]
+        lp = item["mean"][n] - item["std"][n]
+        plt.plot([idx, idx], [lp, hp], "k")
+        plt.plot([idx, ], [item["mean"][n], ], "ok")
+plt.xticks(np.arange(len(weights_stats)), [_["name"] for _ in weights_stats], rotation='vertical')
+plt.grid()
+plt.tight_layout()
+plt.savefig(filename, dpi=300, bbox_inches=None)
+plt.clf()
+plt.close()
